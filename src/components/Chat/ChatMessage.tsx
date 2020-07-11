330435d97b921a6059ca2b359558e34fa5903649
Fausto Rendon <contact@faustorendon.com>
2020-07-11 16:18:34 -0700

    initial styling

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 interface IChatMessage {
 	username: string;
 	message: string;
+	self: string;
+	selfColor: string;
 }
+const messageStyle: CSSProperties = {
+	alignSelf: 'flex-end',
+	width: '70%',
+	backgroundColor: '#7e57c2',
+	border: '1px solid #d8d6d6',
+	padding: '4px 8px',
+	borderRadius: '5px',
+	margin: '5px 0',
+	color: '#ffffff',
+};
+const messageUsernameSelfStyle: CSSProperties = {
+	fontWeight: 'bold',
+	color: '#7e57c2',
+};
+const messageTextStyle: CSSProperties = {
+	fontSize: '1.2rem',
+	whiteSpace: 'pre-line',
+	overflowWrap: 'break-word',
+};
 
-const ChatMessage = ({ username, message }: IChatMessage): JSX.Element => {
+const ChatMessage = ({ username, message, self, selfColor }: IChatMessage): JSX.Element => {
+	const messageSelfStyle: CSSProperties = {
+		alignSelf: 'flex-start',
+		width: '70%',
+		backgroundColor: selfColor,
+		border: '1px solid #d8d6d6',
+		padding: '4px 8px',
+		borderRadius: '5px',
+		margin: '5px 0',
+		color: '#344244',
+	};
+	const messageUsernameStyle: CSSProperties = {
+		fontWeight: 'bold',
+		color: selfColor,
+	};
 	return (
-		<p>
-			<strong>{username}</strong> {message}
-		</p>
+		<div style={self === username ? messageSelfStyle : messageStyle}>
+			<div style={self === username ? messageUsernameSelfStyle : messageUsernameStyle}>{username}</div>
+			<div style={messageTextStyle}>{message}</div>
+		</div>
 	);
 };
 
---
+++ src/components/Chat/Chatroom.tsx
@@ -0,0 +1,48 @@
+import React, { CSSProperties } from 'react';
+import ChatInput from './ChatInput';
+import ChatHistory from './ChatHistory';
+import ChatHeader from './ChatHeader';
+import { IChatroomMessage, IChatroomDetails } from '../../interfaces';
+
+interface IChatroom {
+	chatroomName: string;
+	username: string;
+	chatHistory: IChatroomMessage[];
+	selectedChatroom: IChatroomDetails | null;
+	onLeaveChatroom: (username: string, chatroomName: string) => void;
+	onSubmitChatMessage(message: string): void;
+}
+
+const chatroomStyle: CSSProperties = {
+	// padding: '.75rem',
+	borderRadius: '1rem 1rem 1rem 1rem',
+};
+
+const Chatroom = ({
+	username,
+	chatHistory,
+	selectedChatroom,
+	onLeaveChatroom,
+	onSubmitChatMessage,
+}: IChatroom): JSX.Element | null => {
+	if (!selectedChatroom?.name || !username) {
+		return null;
+	}
+	return (
+		<div className="row margin-top-50">
+			<div className="col s12 m8 xl6 offset-m2 offset-xl3">
+				<div className=" z-depth-2" style={chatroomStyle}>
+					<ChatHeader
+						username={username}
+						selectedChatroom={selectedChatroom}
+						onLeaveChatroom={onLeaveChatroom}
+					/>
+					<ChatHistory chatHistory={chatHistory} selectedChatroom={selectedChatroom} self={username} />
+					<ChatInput onSubmitChatMessage={onSubmitChatMessage} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Chatroom;
